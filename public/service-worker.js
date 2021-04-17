6fbc9293028121a19e848e5f46dc6cbc24794dba
emilykidd3 <e.kidd61@yahoo.com>
2021-04-16 19:53:42 -0700

    added content to idb, and manifest.json

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,22 @@
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
+const DATA_CACHE_NAME = 'data-cache-' + VERSION;
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "/index.html",
   "/css/styles.css",
   "/js/index.js",
   "/js/idb.js",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/manifest.json"
 ];
-
-// Respond with cached resources
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
-  if (e.request.url.indexOf("/api/transaction") !== -1) {
-    e.respondWith(
-      caches.open(CACHE_NAME).then(function (request) {
-        if (request.status === 200) {
-          // if cache is available, respond with cache
-          console.log("responding with cache : " + e.request.url);
-          return request;
-        } else {
-          // if there are no cache, try fetching request
-          console.log("file is not cached, fetching : " + e.request.url);
-          return fetch(e.request);
-        }
-
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    );
-  }
-});
 
 // Cache resources
 self.addEventListener("install", function (e) {
@@ -37,6 +24,41 @@ self.addEventListener("install", function (e) {
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Respond with cached resources
+self.addEventListener("fetch", function (e) {
+  if (e.request.url.includes("/api/")) {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(e.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
     })
   );
 });
